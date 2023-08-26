a18e3523d1b0c68a7db0f7851e2664e6e03c4f35
ovtt17 <ovettmora17@gmail.com>
2023-08-25 22:50:37 -0600

    first commit

---
+++ src/main/java/com/mycompany/heycha/HeyCha.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.heycha;
+
+/**
+ *
+ * @author Ovett
+ */
+public class HeyCha {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
